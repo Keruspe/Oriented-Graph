713176f8e70b3d79ebc880149605060d7df5455f
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-10-12 16:50:07 +0200

    add ponderation and namespace
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- graph.hh
+++ graph.hh
@@ -4,13 +4,15 @@
 #include <string>
 
 using std::string;
+using std::map;
+using std::pair;
 
 class Graph
 {
 public:
     virtual void add_node (string &label) = 0;
     virtual void delete_node (string &label) = 0;
-    virtual void add_arc (string &label, string &from, string &to) = 0;
+    virtual void add_arc (string &label, int ponderation, string &from, string &to) = 0;
     virtual void remove_arc (string &label) = 0;
     virtual void list_successors (string &label) = 0;
     virtual void list_ancestors (string &label) = 0;
@@ -18,6 +20,9 @@ public:
     virtual void list_arcs_to (string &label) = 0;
     virtual void node_count () = 0;
     virtual void arcs_count () = 0;
+private:
+    map<string, int> nodes; /* label -> id */
+    map<string, pair<int, int> > arcs; /* label -> id/ponderation */
 };
 
 #endif /*__GRAPH_HH__*/
