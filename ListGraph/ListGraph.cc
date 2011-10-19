f0b2c7b76ad162f4b72a745fd24f9bfa78890e37
gwenp <gwenael.pluchon@gmail.com>
2011-10-19 20:08:07 +0200

    optimisation des recherches dans les listes (bool found)

--- ListGraph/ListGraph.cc
+++ ListGraph/ListGraph.cc
@@ -7,30 +7,51 @@ ListGraph::ListGraph() : Graph()
 
 int ListGraph::add_node () 
 {
-	list_links.push_back(ListGraphItem(list_links.size()));
+	int r = list_links.size();
+	list_links.push_back(ListGraphItem(r));
+	return r;
 }
 
 void ListGraph::delete_node (int id) 
 {
-	bool trouve = false;
+	bool found = false;
 
 	std::list<ListGraphItem>::iterator it=list_links.begin();
 	
-	while(trouve == false && it != list_links.end())
+	while(found == false && it != list_links.end())
 	{
 		if(it->id == id)
+		{
 			list_links.erase(it);
+			found = true;
+		}
 	}
 }
 
 int ListGraph::add_arc (int from, int to)
-{}
+{
+	bool found = false;
+
+	std::list<ListGraphItem>::iterator it=list_links.begin();
+	
+	while(found == false && it != list_links.end())
+	{
+		if(it->id == to)
+		{
+			it->listLinkedArcs->push_back(from);
+			found = true;
+		}
+	}
+}
 
 void ListGraph::remove_arc (int id)
-{}
+{
+	//probleme de O(N*M)? (N=list_links.size() && M = listLinkedArcs.size())
+}
 
 set<int> ListGraph::list_successors (int id)
-{}
+{
+}
 
 set<int> ListGraph::list_ancestors (int id)
 {}
