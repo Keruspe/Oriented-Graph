e307642013905eec90c4a8eb8cebcfd0faaa85c1
gwenp <gwenael.pluchon@gmail.com>
2011-10-19 19:53:49 +0200

    added ListGraph

---
+++ ListGraph/ListGraph.hh
@@ -0,0 +1,15 @@
+#ifndef __LISTGRAPH_HH__
+#define __LISTGRAPH_HH__
+
+#include "../graph.hh"
+
+class ListGraph : public Graph
+{
+	public:
+		ListGraph();
+			
+	private:
+		/* add your private declarations */
+};
+
+#endif /* LISTGRAPH_HH */ 
