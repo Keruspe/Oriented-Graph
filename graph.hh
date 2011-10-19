f4d99c7a92ba354cb616240d0c016c3d101ffbb9
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2011-10-19 15:43:17 +0200

    add missing stuff
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- graph.hh
+++ graph.hh
@@ -14,13 +14,17 @@ public:
     virtual void remove_arc (int id) = 0;
     virtual set<int> list_successors (int id) = 0;
     virtual set<int> list_ancestors (int id) = 0;
+    virtual set<int> list_nodes () = 0;
     virtual set<int> list_arcs_from (int id) = 0;
     virtual set<int> list_arcs_to (int id) = 0;
+    virtual set<int> list_arcs () = 0;
     virtual int node_count () = 0;
     virtual int arcs_count () = 0;
 private:
     int next_node_id;
+    int get_next_node_id () { return next_node_id++; }
     int next_arc_id;
+    int get_next_arc_id () { return next_arc_id++; }
 };
 
 #endif /*__GRAPH_HH__*/
