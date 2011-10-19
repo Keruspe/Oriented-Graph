8abee74085ddb6db14679034a9b78653a5d36259
gwenp <gwenael.pluchon@gmail.com>
2011-10-19 19:58:23 +0200

    ListGraph::add_node && ListGraph::delete_node ok

--- ListGraph/ListGraph.cc
+++ ListGraph/ListGraph.cc
@@ -1,9 +1,56 @@
 #include "ListGraph.hh"
 
-
-ListGraph::ListGraph()
+ListGraph::ListGraph() : Graph()
 {
 	
 }
+
+int ListGraph::add_node () 
+{
+	list_links.push_back(ListGraphItem(list_links.size()));
+}
+
+void ListGraph::delete_node (int id) 
+{
+	bool trouve = false;
+
+	std::list<ListGraphItem>::iterator it=list_links.begin();
+	
+	while(trouve == false && it != list_links.end())
+	{
+		if(it->id == id)
+			list_links.erase(it);
+	}
+}
+
+int ListGraph::add_arc (int from, int to)
+{}
+
+void ListGraph::remove_arc (int id)
+{}
+
+set<int> ListGraph::list_successors (int id)
+{}
+
+set<int> ListGraph::list_ancestors (int id)
+{}
+
+set<int> ListGraph::list_arcs_from (int id)
+{}
+
+set<int> ListGraph::list_arcs()
+{}
+
+set<int> ListGraph::list_nodes()
+{}
+
+set<int> ListGraph::list_arcs_to (int id)
+{}
+
+int ListGraph::node_count () 
+{}
+
+int ListGraph::arcs_count () 
+{}
 
 
--- ListGraph/ListGraph.hh
+++ ListGraph/ListGraph.hh
@@ -1,15 +1,40 @@
 #ifndef __LISTGRAPH_HH__
 #define __LISTGRAPH_HH__
 
+#include <list>
 #include "../graph.hh"
+
+
 
 class ListGraph : public Graph
 {
 	public:
 		ListGraph();
-			
+		virtual int add_node () ;
+		virtual void delete_node (int id) ;
+		virtual int add_arc (int from, int to) ;
+		virtual void remove_arc (int id) ;
+		virtual set<int> list_successors (int id) ;
+		virtual set<int> list_ancestors (int id) ;
+		virtual set<int> list_arcs_from (int id) ;
+		virtual set<int> list_arcs() ;
+		virtual set<int> list_nodes() ;
+		virtual set<int> list_arcs_to (int id) ;
+		virtual int node_count () ;
+		virtual int arcs_count () ;
+
 	private:
-		/* add your private declarations */
+	
+		class ListGraphItem
+		{
+			public:
+				ListGraphItem(int i){id=i;};
+				int id;//pas besoin => iterateur
+				std::list<int> listLinkedArcs;
+		};
+		std::list<ListGraphItem> list_links;
+		
+
 };
 
 #endif /* LISTGRAPH_HH */ 
